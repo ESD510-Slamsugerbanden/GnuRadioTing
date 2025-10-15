922ef89993f291d04cb3468f085c841e5899383b
Maltheren <supermalthe1000@gmail.com>
2025-10-15 15:02:04 +0200

    transmitter

---
+++ Walsh/Transfrans.py
@@ -0,0 +1,236 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: Not titled yet
+# Author: sennels
+# GNU Radio version: 3.10.9.2
+
+from PyQt5 import Qt
+from gnuradio import qtgui
+from gnuradio import blocks
+from gnuradio import filter
+from gnuradio.filter import firdes
+from gnuradio import gr
+from gnuradio.fft import window
+import sys
+import signal
+from PyQt5 import Qt
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+from gnuradio import uhd
+import time
+import sip
+
+
+
+class Transfrans(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Not titled yet")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except BaseException as exc:
+            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "Transfrans")
+
+        try:
+            geometry = self.settings.value("geometry")
+            if geometry:
+                self.restoreGeometry(geometry)
+        except BaseException as exc:
+            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samples_pr_sym = samples_pr_sym = 5
+        self.Symbol_rate = Symbol_rate = 100
+        self.sep_freq = sep_freq = 20000
+        self.samp_rate = samp_rate = 500000
+        self.PI = PI = 3.14159265358979323
+        self.Base_samp_rate = Base_samp_rate = Symbol_rate*samples_pr_sym
+
+        ##################################################
+        # Blocks
+        ##################################################
+
+        self.uhd_usrp_sink_0 = uhd.usrp_sink(
+            ",".join(("", '')),
+            uhd.stream_args(
+                cpu_format="fc32",
+                args='',
+                channels=[0],
+            ),
+            "",
+        )
+        self.uhd_usrp_sink_0.set_samp_rate(samp_rate)
+        self.uhd_usrp_sink_0.set_time_now(uhd.time_spec(time.time()), uhd.ALL_MBOARDS)
+
+        self.uhd_usrp_sink_0.set_center_freq(2410000000, 0)
+        self.uhd_usrp_sink_0.set_antenna("TX/RX", 0)
+        self.uhd_usrp_sink_0.set_normalized_gain(1, 0)
+        self.qtgui_waterfall_sink_x_0 = qtgui.waterfall_sink_c(
+            1024, #size
+            window.WIN_BLACKMAN_hARRIS, #wintype
+            0, #fc
+            samp_rate, #bw
+            "", #name
+            1, #number of inputs
+            None # parent
+        )
+        self.qtgui_waterfall_sink_x_0.set_update_time(0.10)
+        self.qtgui_waterfall_sink_x_0.enable_grid(False)
+        self.qtgui_waterfall_sink_x_0.enable_axis_labels(True)
+
+
+
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        colors = [0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_waterfall_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_waterfall_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_waterfall_sink_x_0.set_color_map(i, colors[i])
+            self.qtgui_waterfall_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self.qtgui_waterfall_sink_x_0.set_intensity_range(-140, 10)
+
+        self._qtgui_waterfall_sink_x_0_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_0.qwidget(), Qt.QWidget)
+
+        self.top_layout.addWidget(self._qtgui_waterfall_sink_x_0_win)
+        self.blocks_vector_source_x_0 = blocks.vector_source_f((1, -1, 1, -1, 1, -1, 1,-1), True, 1, [])
+        self.blocks_vco_c_0_0 = blocks.vco_c(samp_rate, (2*PI), 1)
+        self.blocks_repeat_0 = blocks.repeat(gr.sizeof_float*1, (int((samp_rate)/(Symbol_rate))))
+        self.blocks_multiply_const_vxx_0_0 = blocks.multiply_const_ff(sep_freq)
+        self.band_pass_filter_0_0_0 = filter.fir_filter_ccf(
+            1,
+            firdes.band_pass(
+                1,
+                samp_rate,
+                (sep_freq-1000),
+                (sep_freq+1000),
+                8000,
+                window.WIN_HAMMING,
+                6.76))
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.band_pass_filter_0_0_0, 0), (self.qtgui_waterfall_sink_x_0, 0))
+        self.connect((self.band_pass_filter_0_0_0, 0), (self.uhd_usrp_sink_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0_0, 0), (self.blocks_vco_c_0_0, 0))
+        self.connect((self.blocks_repeat_0, 0), (self.blocks_multiply_const_vxx_0_0, 0))
+        self.connect((self.blocks_vco_c_0_0, 0), (self.band_pass_filter_0_0_0, 0))
+        self.connect((self.blocks_vector_source_x_0, 0), (self.blocks_repeat_0, 0))
+
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "Transfrans")
+        self.settings.setValue("geometry", self.saveGeometry())
+        self.stop()
+        self.wait()
+
+        event.accept()
+
+    def get_samples_pr_sym(self):
+        return self.samples_pr_sym
+
+    def set_samples_pr_sym(self, samples_pr_sym):
+        self.samples_pr_sym = samples_pr_sym
+        self.set_Base_samp_rate(self.Symbol_rate*self.samples_pr_sym)
+
+    def get_Symbol_rate(self):
+        return self.Symbol_rate
+
+    def set_Symbol_rate(self, Symbol_rate):
+        self.Symbol_rate = Symbol_rate
+        self.set_Base_samp_rate(self.Symbol_rate*self.samples_pr_sym)
+        self.blocks_repeat_0.set_interpolation((int((self.samp_rate)/(self.Symbol_rate))))
+
+    def get_sep_freq(self):
+        return self.sep_freq
+
+    def set_sep_freq(self, sep_freq):
+        self.sep_freq = sep_freq
+        self.band_pass_filter_0_0_0.set_taps(firdes.band_pass(1, self.samp_rate, (self.sep_freq-1000), (self.sep_freq+1000), 8000, window.WIN_HAMMING, 6.76))
+        self.blocks_multiply_const_vxx_0_0.set_k(self.sep_freq)
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.band_pass_filter_0_0_0.set_taps(firdes.band_pass(1, self.samp_rate, (self.sep_freq-1000), (self.sep_freq+1000), 8000, window.WIN_HAMMING, 6.76))
+        self.blocks_repeat_0.set_interpolation((int((self.samp_rate)/(self.Symbol_rate))))
+        self.qtgui_waterfall_sink_x_0.set_frequency_range(0, self.samp_rate)
+        self.uhd_usrp_sink_0.set_samp_rate(self.samp_rate)
+
+    def get_PI(self):
+        return self.PI
+
+    def set_PI(self, PI):
+        self.PI = PI
+
+    def get_Base_samp_rate(self):
+        return self.Base_samp_rate
+
+    def set_Base_samp_rate(self, Base_samp_rate):
+        self.Base_samp_rate = Base_samp_rate
+
+
+
+
+def main(top_block_cls=Transfrans, options=None):
+
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+
+    tb.start()
+
+    tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
+
+    qapp.exec_()
+
+if __name__ == '__main__':
+    main()
