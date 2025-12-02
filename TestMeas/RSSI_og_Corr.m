be7e10aa3d5622bc7eb3124327bb9d64c6909357
aolss23 <159021345+aolss23@users.noreply.github.com>
2025-12-02 13:39:18 +0100

    MatlabKode

--- TestMeas/RSSI_og_Corr.m
+++ TestMeas/RSSI_og_Corr.m
@@ -11,23 +11,35 @@ r = fopen('RSSI.bin', 'rb');
 RSSI = fread(r, Inf, 'float');
 
 
+Fs = 250e3;
+N = length(RSSI);
+t = (0:N-1)' / Fs;
+N2 = length(Correlation);
+t2 = (0:N2-1)' / Fs;
+t_us    = t * 1e6;  
+t_us2 = t2*1e6;
+% t_ms    = t * 1e3; 
+
 figure;
 
 % Hovedplot
-plot(RSSI)
+plot(t, RSSI)
 hold on
-plot(Correlation)
-xlim([98e3 132e3])
+plot(t2, Correlation)
+xlim([1.04742 1.18698])
 legend('RSSI', 'Correlation')
-rectangle('Position', [119.9e3, -1, (121e3-119.9e3), (1+0.6)], ...
+xlabel('Time [s]', 'Interpreter','latex')
+ylabel('RSSI and Correlation value', Interpreter='latex')
+rectangle('Position', [1.1706, -1, (1.17391-1.1706), (1+0.6)], ...
           'EdgeColor', 'k', 'LineStyle', '--', 'LineWidth', 1.2);
 
 % Zoom-indholdet
-axInset = axes('Position',[0.5 0.55 0.25 0.25]);
+axInset = axes('Position',[0.64 0.55 0.22 0.22]);
 
-plot(RSSI)
+plot(t, RSSI)
 hold on
-plot(Correlation)
-xlim([119.9e3 121e3])     % Det område du vil zoome ind på
+plot(t2, Correlation)
+xlim([1.1706 1.17391])     % Det område du vil zoome ind på
 title('Zoom')
+%xlabel(axInset, 'time [us]', Interpreter='latex')
 set(axInset, 'Box','on')
